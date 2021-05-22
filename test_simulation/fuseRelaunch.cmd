575394613ecca6108959c7c0092a0a8846e609d6
Thomas Nguyen <thomasngn673@gmail.com>
2021-05-22 15:14:39 -0400

    deleted test_simulation

--- test_simulation/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "C:/Users/Thomas Nguyen/Desktop/Verilog/test_simulation/stimulus_isim_beh.exe" -prj "C:/Users/Thomas Nguyen/Desktop/Verilog/test_simulation/stimulus_beh.prj" "work.stimulus" "work.glbl" 
